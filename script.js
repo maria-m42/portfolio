80f332c0888bcea00684ce2b1a6c766a8b1f28ae
nataliem-m <98995843+nataliem-m@users.noreply.github.com>
2026-04-18 19:39:48 -0500

    added about me

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleMenu() {
     const navList = document.getElementById('nav-list');
     navList.classList.toggle('active');
 }
+// Optional: Closes the menu when a link is clicked
+document.querySelectorAll('.nav-items a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('nav-list').classList.remove('active');
+    });
+});
