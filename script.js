834718741daec7f7aacc61f2e6d016b67ad7a5ac
nataliem-m <98995843+nataliem-m@users.noreply.github.com>
2026-05-01 20:49:37 -0500

    contact me done

--- script.js
+++ script.js
@@ -2,9 +2,34 @@ function toggleMenu() {
     const navList = document.getElementById('nav-list');
     navList.classList.toggle('active');
 }
-// Optional: Closes the menu when a link is clicked
+
 document.querySelectorAll('.nav-items a').forEach(link => {
     link.addEventListener('click', () => {
         document.getElementById('nav-list').classList.remove('active');
     });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const btn = document.getElementById('submit-button');
+    btn.innerText = 'SENDING...';
+
+    const serviceID = 'service_t3hlvru';
+    const templateID = 'template_8f9i74u';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.innerText = 'MESSAGE SENT!';
+            
+            btn.classList.add('sent-success'); 
+            
+            btn.disabled = true; 
+            /* ----------------------- */
+            document.getElementById('contact-form').reset();
+            
+        }, (err) => {
+            btn.innerText = 'FAILED...';
+            console.log(JSON.stringify(err));
+        });
 });
